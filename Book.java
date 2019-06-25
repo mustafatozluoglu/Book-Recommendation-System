e795ecd4d0178a0096408f7fdbff44e8f42657f0
Theieyrre <crizfmtfm@gmail.com>
2019-06-25 22:44:14 +0300

    Closes #2 and changing fetching

--- Book.java
+++ Book.java
@@ -1,125 +1,61 @@
-import java.util.StringTokenizer;
-
 public class Book {
     private String name;
     private String publisher;
     private String author;
-    private String details;
     private double priceOther;
     private double priceSite;
     private int pageCount;
+    private int saleCount;
+    private String genre;
+    private double point;
+    private int votes;
 
-    public Book(String bookName, String bookPublisher, String bookAuthor, String bookDetails, double bookPriceOther, double bookPriceSite) {
+    public Book(String bookName, String bookPublisher, String bookAuthor, double bookPriceOther, double bookPriceSite, int bookSaleCount, String bookGenre, double bookPoint, int bookVotes) {
         name = bookName;
         publisher = bookPublisher;
         author = bookAuthor;
-        details = bookDetails;
         priceOther = bookPriceOther;
         priceSite = bookPriceSite;
-        parseDetails();
+        saleCount = bookSaleCount;
+        genre = bookGenre;
+        point = bookPoint;
+        votes = bookVotes;
     }
 
-    public void setName(String bookName) {
-        name = bookName;
-    }
+    public void setName(String bookName) {        name = bookName;    }
+    public void setPublisher(String bookPublisher) {        publisher = bookPublisher;    }
+    public void setauthor(String bookAuthor) {        author = bookAuthor;    }
+    public void setPriceOther(int bookPriceOther) {        priceOther = bookPriceOther;    }
+    public void setPriceSite(int bookPriceSite) {        priceSite = bookPriceSite;    }
+    public void setPagecount(int bookPageCount) {        pageCount = bookPageCount;    }
+    public void setSaleCount(int bookSaleCount) {	saleCount = bookSaleCount; }
+    public void setGenre(String bookGenre) { genre = bookGenre; }
+    public void setPoints(double bookPoint) { point = bookPoint; }
+    public void setVotes(int bookVotes) { votes = bookVotes; }
 
-    public void setPublisher(String bookPublisher) {
-        publisher = bookPublisher;
-    }
-
-    public void setauthor(String bookAuthor) {
-        author = bookAuthor;
-    }
-
-    public void setDetails(String bookDetails) {
-        details = bookDetails;
-    }
-
-    public void setPriceOther(int bookPriceOther) {
-        priceOther = bookPriceOther;
-    }
-
-    public void setPriceSite(int bookPriceSite) {
-        priceSite = bookPriceSite;
-    }
-
-    public void setPagecount(int bookPageCount) {
-        pageCount = bookPageCount;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getDetails() {
-        return details;
-    }
-
-    public double getPriceOther() {
-        return priceOther;
-    }
-
-    public double getPriceSite() {
-        return priceSite;
-    }
-
-    public int getPageCount() {
-        return pageCount;
-    }
-
-    //Method to parse details into meaningful sentence
-    public String parseDetails() {
-        //Example format of details
-        // 6053609902 | 18. Bask� | T�RK�E | 136 | Karton Kapak | Kitap Ka��d� 2019-05-24
-        //System.out.println(details);
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer tokenizer = new StringTokenizer(details, "|");
-        String token = tokenizer.nextToken();
-        if (!Character.isDigit(token.charAt(1))) //For detail that doesnt fit example format
-            token = tokenizer.nextToken(); //Ignore that token
-        //System.out.println(token);
-        sb.append("ID of book: " + token);
-        token = tokenizer.nextToken();
-        //System.out.println(token);
-        sb.append("\nEdition number: " + token);
-        token = tokenizer.nextToken();
-        //System.out.println(token);
-        sb.append("\nLanguage: " + token);
-        token = tokenizer.nextToken();
-
-        if (!Character.isDigit(token.charAt(1)))
-            pageCount = 0;
-        else {
-            token = token.substring(1, token.length() - 1);
-            pageCount = Integer.parseInt(token); //Needs validation
-            token = tokenizer.nextToken();
-        }
-        //System.out.println(token);
-        sb.append("\nPage count: " + pageCount);
-        //System.out.println(token);
-        sb.append("\nCover: " + token);
-        token = tokenizer.nextToken();
-        //System.out.println(token);
-        sb.append("\nPage Quality and publish date: " + token);
-        return sb.toString();
-    }
+    public String getName() {        return name;    }
+    public String getPublisher() {        return publisher;    }
+    public String getAuthor() {        return author;    }
+    public double getPriceOther() {        return priceOther;    }
+    public double getPriceSite() {        return priceSite;    }
+    public int getPageCount() {        return pageCount;    }
+    public int getSaleCount() { return saleCount; }
+    public String getGenre() { return genre; }
+    public double getPoint() { return point; }
+    public int getVotes() { return votes; }
+    
 
     public String toString() {
         return "--------------------------------------------\n"
                 + "Name of the book: " + name
                 + "\nPublisher: " + publisher
                 + "\nAuthor: " + author
-                + "\n" + parseDetails()
                 + "\nPrice in other sites: " + priceOther
                 + "\nPrice in Kitapyurdu: " + priceSite
+                + "\nTotal number of sales: " + saleCount
+        		+ "\nGenre of book: " + genre
+        		+ "\nTotal point of users: " + point
+        		+ "\nTotal votes: " + votes
                 + "\n--------------------------------------------\n";
     }
 }
--- FetchBook.java
+++ FetchBook.java
@@ -4,67 +4,69 @@ import java.util.ArrayList;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
+
+/* Last change date 25.06.2019 22:42
+ * Could not retrieve overall score of votes
+ */
 
 public class FetchBook {
     public static void main(String args[]) throws IOException {
-
-        String url = "https://www.kitapyurdu.com/index.php?route=product/best_sellers&list_id=1&filter_in_stock=1&filter_in_stock=1&page=1";
-        String temp = Jsoup.connect("https://www.kitapyurdu.com/index.php?route=product/best_sellers&list_id=1&filter_in_stock=1&filter_in_stock=1&page=1").get().select("div.results").text();
+    	
+    	//URL of the Best seller page on Kitapyurdu without page counter
+        String url = "https://www.kitapyurdu.com/index.php?route=product/best_sellers&list_id=1&filter_in_stock=1&filter_in_stock=1&page=";
+        
+        //Opening first page to get total page number
+        String temp = Jsoup.connect(url+1).get().select("div.results").text();
         temp = temp.substring(temp.indexOf("(")+1,temp.lastIndexOf(" "));
         int totalPageCount = Integer.parseInt(temp);
+        
         //Arraylist to store all books
         ArrayList<Book> booklist = new ArrayList<>();
 
-        for (int pageCount = 1; pageCount < totalPageCount; pageCount++) {
-
-
-            //First page of Best Seller on kitapyurdu.com
-            Document bestSeller = Jsoup.connect("https://www.kitapyurdu.com/index.php?route=product/best_sellers&list_id=1&filter_in_stock=1&filter_in_stock=1&page=" + pageCount).get();
+        for (int webPageCount = 1; webPageCount < totalPageCount; webPageCount++) {
+        	
+        	//Connect to page number pageCount
+            Document bestSeller = Jsoup.connect(url + webPageCount).get();
 
             //Each book has itemtype Book
             Elements allBooks = bestSeller.select("div[itemtype=\"http://schema.org/Book\"]");
-
-
+ 
+            //Elements class does not have hasNext() so loop through it with for and size
             for (int index = 0; index < allBooks.size(); index++) {
                 //Temporary variables to make code more readable
-                String name, publisher, author, details;
-                double priceOther, priceSite;
+                String name, publisher, author, genre;
+                double priceOther, priceSite, point;
+                int pageCount, saleCount, votes;
+                
+                Element currentBook = allBooks.first();
+                String urlOfBook = currentBook.select("div.name").select("a[href]").attr("href");
+                //Create new document to get datas from books individual site
+                Document bookURL = Jsoup.connect(urlOfBook).get();
+                
+                // For an example book Ana sayfa -> Book name
+                name = bookURL.select("div.breadcrumb a[href]").eq(1).text();
+                author = bookURL.select("span[itemprop=author]").text();
+                publisher = bookURL.select("span[itemprop=publisher] span[itemprop=name]").text();
+                
+                // Genre is stored on site as Kitap » Diðer » Kiþisel Geliþim 
+                String tempForParsing = bookURL.select("div.grid_6.omega.alpha.section a[href]").text();
+                genre = tempForParsing.substring(tempForParsing.lastIndexOf("»")+1);
+                
+                //Price is stored such as "27,50"
+                tempForParsing = bookURL.select("div.list_price span[class=value]").text();
+                tempForParsing = tempForParsing.replaceAll(",",".");
+                priceOther = Double.parseDouble(tempForParsing);
+                priceSite =  Double.parseDouble(bookURL.select("div[itemprop=offers] meta[itemprop=price]").attr("content"));
+                
+                tempForParsing = bookURL.select("div.f1.mg-r-10.mg-b-5").text();
+                //point = Double.parseDouble(.text());
+                System.out.println(tempForParsing);
+                //System.out.println(bookURL);
 
-                //Assign values of them
-                name = allBooks.get(index).child(4).text();
-                if(!name.contains("Dergi") && allBooks.get(index).children().size()==16){
-                    publisher = allBooks.get(index).child(5).child(0).text();
-                    author = allBooks.get(index).child(7).text();
-                    details = allBooks.get(index).child(8).text();
+            }//end of fetching books from site 
+        } 
 
-                    //Child(9) indicates prices, Child(1) indicates price in other site with text "Liste fiyat?" Child(1) of that is only price
-                    //Same hierarchy exist for Kitapyurdu price
-                    String temporary = allBooks.get(index).child(9).child(0).child(1).text();
-                    temporary = temporary.replace(',', '.'); //Prices include comma to seperate penny
-                    priceOther = Double.parseDouble(temporary); //Needs validaiton
-                    temporary = allBooks.get(index).child(9).child(1).child(1).text();
-                    temporary = temporary.replace(',', '.');
-                    priceSite = Double.parseDouble(temporary); //Needs valdiation as well
-
-                    //System.out.println(name); //Name of the book
-                    //System.out.println(publisher); //Publisher of the book
-                    //System.out.println(author); //Author of the book
-                    //System.out.println(details); //Book details
-
-                    //System.out.println(priceOther); //Price of the book in other sites
-                    //System.out.println(priceSite); //Price of the book in Kitapyurdu
-
-                    //Create temporary book object to write these data to booklist
-                    Book newest = new Book(name, publisher, author, details, priceOther, priceSite);
-                    booklist.add(newest);
-                }
-
-            }//end of fetching books from site
-        }
-
-
-
-        System.out.println(booklist.size()); // To see if book is added correctly
 
     }//end of main method
 }
