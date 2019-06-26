ef6a9dae97c2a45a3ed87edb6fdccf4e46c7ce4e
mustafatozluoglu <m_saidt@hotmail.com>
2019-06-26 23:37:32 +0300

    Fixed "saleCount" Exception.

--- Book.java
+++ Book.java
@@ -23,28 +23,86 @@ public class Book {
         votes = bookVotes;
     }
 
-    public void setName(String bookName) {        name = bookName;    }
-    public void setPublisher(String bookPublisher) {        publisher = bookPublisher;    }
-    public void setauthor(String bookAuthor) {        author = bookAuthor;    }
-    public void setPriceOther(int bookPriceOther) {        priceOther = bookPriceOther;    }
-    public void setPriceSite(int bookPriceSite) {        priceSite = bookPriceSite;    }
-    public void setPagecount(int bookPageCount) {        pageCount = bookPageCount;    }
-    public void setSaleCount(int bookSaleCount) {	saleCount = bookSaleCount; }
-    public void setGenre(String bookGenre) { genre = bookGenre; }
-    public void setPoints(int bookPoint) { point = bookPoint; }
-    public void setVotes(int bookVotes) { votes = bookVotes; }
+    public void setName(String bookName) {
+        name = bookName;
+    }
 
-    public String getName() {        return name;    }
-    public String getPublisher() {        return publisher;    }
-    public String getAuthor() {        return author;    }
-    public double getPriceOther() {        return priceOther;    }
-    public double getPriceSite() {        return priceSite;    }
-    public int getPageCount() {        return pageCount;    }
-    public int getSaleCount() { return saleCount; }
-    public String getGenre() { return genre; }
-    public int getPoint() { return point; }
-    public int getVotes() { return votes; }
-    
+    public void setPublisher(String bookPublisher) {
+        publisher = bookPublisher;
+    }
+
+    public void setauthor(String bookAuthor) {
+        author = bookAuthor;
+    }
+
+    public void setPriceOther(int bookPriceOther) {
+        priceOther = bookPriceOther;
+    }
+
+    public void setPriceSite(int bookPriceSite) {
+        priceSite = bookPriceSite;
+    }
+
+    public void setPagecount(int bookPageCount) {
+        pageCount = bookPageCount;
+    }
+
+    public void setSaleCount(int bookSaleCount) {
+        saleCount = bookSaleCount;
+    }
+
+    public void setGenre(String bookGenre) {
+        genre = bookGenre;
+    }
+
+    public void setPoints(int bookPoint) {
+        point = bookPoint;
+    }
+
+    public void setVotes(int bookVotes) {
+        votes = bookVotes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPriceOther() {
+        return priceOther;
+    }
+
+    public double getPriceSite() {
+        return priceSite;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public int getSaleCount() {
+        return saleCount;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
 
     public String toString() {
         return "--------------------------------------------\n"
@@ -55,15 +113,15 @@ public class Book {
                 + "\nPrice in Kitapyurdu: " + priceSite
                 + "\nTotal number of pages: " + pageCount
                 + "\nTotal number of sales: " + saleCount
-        		+ "\nGenre of book: " + genre
-        		+ "\nTotal point of users: " + point
-        		+ "\nTotal votes: " + votes
+                + "\nGenre of book: " + genre
+                + "\nTotal point of users: " + point
+                + "\nTotal votes: " + votes
                 + "\n--------------------------------------------\n";
     }
-    
+
     public String toData() {
-    	return "'"+name+"','"+publisher+"','"+author+"',"+priceOther+","+priceSite+","+pageCount+","+saleCount+",'"+genre+"',"+point+","+votes+",";
+        return "'" + name + "','" + publisher + "','" + author + "'," + priceOther + "," + priceSite + "," + pageCount + "," + saleCount + ",'" + genre + "'," + point + "," + votes + ",";
     }
-    	
+
 }
 
--- FetchBook.java
+++ FetchBook.java
@@ -8,74 +8,76 @@ import org.jsoup.nodes.Element;
 
 public class FetchBook {
     public static void main(String args[]) throws IOException {
-    	
-    	//URL of the Best seller page on Kitapyurdu without page counter
+
+        //URL of the Best seller page on Kitapyurdu without page counter
         String url = "https://www.kitapyurdu.com/index.php?route=product/best_sellers&list_id=1&filter_in_stock=1&filter_in_stock=1&page=";
-        
+
         //Opening first page to get total page number
-        String temp = Jsoup.connect(url+1).get().select("div.results").text();
-        temp = temp.substring(temp.indexOf("(")+1,temp.lastIndexOf(" "));
+        String temp = Jsoup.connect(url + 1).get().select("div.results").text();
+        temp = temp.substring(temp.indexOf("(") + 1, temp.lastIndexOf(" "));
         int totalPageCount = Integer.parseInt(temp);
-        
+
         //Arraylist to store all books
         ArrayList<Book> booklist = new ArrayList<>();
 
         for (int webPageCount = 1; webPageCount < totalPageCount; webPageCount++) {
-        	
-        	//Connect to page number pageCount
+
+            //Connect to page number pageCount
             Document bestSeller = Jsoup.connect(url + webPageCount).get();
 
             //Each book has itemtype Book
             Elements allBooks = bestSeller.select("div[itemtype=\"http://schema.org/Book\"]");
- 
+
             //Elements class does not have hasNext() so loop through it with for and size
             for (int index = 0; index < allBooks.size(); index++) {
                 //Temporary variables to make code more readable
                 String name, publisher, author, genre;
                 double priceOther, priceSite;
                 int pageCount, saleCount, votes, point;
-                
+
                 Element currentBook = allBooks.get(index);
                 String urlOfBook = currentBook.select("div.name").select("a[href]").attr("href");
                 //Create new document to get datas from books individual site
                 Document bookURL = Jsoup.connect(urlOfBook).get();
-                
+
                 // For an example book Ana sayfa -> Book name
                 name = bookURL.select("div.breadcrumb a[href]").eq(1).text();
                 author = bookURL.select("span[itemprop=author]").text();
                 publisher = bookURL.select("span[itemprop=publisher] span[itemprop=name]").text();
-                
+
                 // Genre is stored on site as Kitap » Diðer » Kiþisel Geliþim 
                 String tempForParsing = bookURL.select("div.grid_6.omega.alpha.section a[href]").text();
-                genre = tempForParsing.substring(tempForParsing.lastIndexOf("»")+1);
-                
+                genre = tempForParsing.substring(tempForParsing.lastIndexOf("»") + 1);
+
                 //Price is stored such as "27,50"
                 tempForParsing = bookURL.select("div.list_price span[class=value]").text();
-                tempForParsing = tempForParsing.replaceAll(",",".");
+                tempForParsing = tempForParsing.replaceAll(",", ".");
                 priceOther = Double.parseDouble(tempForParsing);
-                priceSite =  Double.parseDouble(bookURL.select("div[itemprop=offers] meta[itemprop=price]").attr("content"));
+                priceSite = Double.parseDouble(bookURL.select("div[itemprop=offers] meta[itemprop=price]").attr("content"));
 
                 //Could only get number of stars
                 point = Integer.parseInt(bookURL.select("meta[itemprop=ratingValue]").attr("content"));
-                
+
                 pageCount = Integer.parseInt(bookURL.select("span[itemprop=numberOfPages]").text());
-                
+
                 tempForParsing = bookURL.select("div.grid_6.omega.alpha.section").text();
-                tempForParsing = tempForParsing.replaceAll("\\D+","");
+                tempForParsing = tempForParsing.replaceAll("\\D+", "");
+                if (tempForParsing.length() > 6)
+                    tempForParsing = tempForParsing.substring(0, 6);
                 saleCount = Integer.parseInt(tempForParsing);
-                
+
                 votes = Integer.parseInt(bookURL.select("meta[itemprop=ratingCount]").attr("content"));
-                Book newest = new Book(name,publisher,author,priceOther,priceSite,pageCount,saleCount,genre,point,votes);
+                Book newest = new Book(name, publisher, author, priceOther, priceSite, pageCount, saleCount, genre, point, votes);
                 booklist.add(newest);
                 System.out.println(newest);
                 System.out.println(newest.toData());
 
             }//end of fetching books from site 
-        } 
-        
+        }
+
         BookComparator<Book> comparator = new BookComparator<>();
         booklist.sort(comparator);
-        
+
 
     }//end of main method
 }
